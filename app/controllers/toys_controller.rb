16b6ad474fe4cdf5398fc88d7e4600ae772c1b91
Nurina Junus <nurina.junus@gmail.com>
2022-04-20 17:23:22 -0600

    commit tree 20e501332cf9b531e8abdde710d87f7bb62a0cd6

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,13 +7,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.errors.full_messages }, status: :unprocessable_entity
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = Toy.find_by_id(params[:id])
     toy.update(toy_params)
+    render json: toy
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.errors.full_messages }, status: :unprocessable_entity
   end
 
   def destroy
@@ -25,7 +30,7 @@ class ToysController < ApplicationController
   private
   
   def toy_params
-    params.permit(:name, :image, :likes)
+    params.permit(:id, :name, :image, :likes)
   end
 
 end
